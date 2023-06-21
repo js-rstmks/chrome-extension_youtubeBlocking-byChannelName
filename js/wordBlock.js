8d442060ce4fe522648cc504fb911b9a167e9ee4
js-rstmks <javascript.skmtsr@gmail.com>
2023-06-20 23:04:53 -0700

    aaa

--- js/wordBlock.js
+++ js/wordBlock.js
@@ -1,7 +1,22 @@
 let siteTitle = document.title
 
+    // googleの検索結果一覧の中にある埋め込みyoutube element
+    // let embeddedYoutubeElement = document.querySelectorAll(".kSFuOd .rkqHyd")
+let embeddedYoutubeElement = document.querySelectorAll(".kSFuOd")
 
-const blockWord = async (videoId) => {
+const googleYoutubeBlock = () => {
+
+    embeddedYoutubeElement.forEach(test => {
+        test.addEventListener("click", () => {
+            window.location.assign(
+                "https://www.investopedia.com/"
+            )
+        })
+    })
+}
+
+
+const blockWord = async () => {
 
     const response2 = await fetch('https://witorz.com/api/return_word', {
         method: 'POST',
@@ -12,6 +27,7 @@ const blockWord = async (videoId) => {
             title: siteTitle,
         })
     })
+    
     const data2 = await response2.text()
     console.log(data2)
 
@@ -23,5 +39,6 @@ const blockWord = async (videoId) => {
     }
 }
 
+googleYoutubeBlock()
 blockWord()
 
